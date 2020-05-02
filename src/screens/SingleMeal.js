4737d1e7350b3e38e64429cd25d40a29d69c8d83
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-05-01 21:17:51 -0700

    edited back single meal to have the touchable opacity heart

--- src/screens/SingleMeal.js
+++ src/screens/SingleMeal.js
@@ -1,8 +1,7 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, Button, Image, Picker } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, Picker, TouchableOpacity } from 'react-native';
 import MealContext from '../context/Context';
 import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 // import { Picker } from '@react-native-community/picker';
 
@@ -19,14 +18,6 @@ const SingleMealScreen = props => {
 
   return (
     <View style={styles.container}>
-      {/* Meal Information and Description */}
-      <View style={styles.infoCard}>
-        <Text>{meal.title}</Text>
-        
-        <TouchableOpacity>
-        <Ionicons name="md-heart" size={32} color="red" />
-        </TouchableOpacity>
-        {/* md-heart-empty */}
       <View style={styles.itemContainer}>
         <Image
           style={{ paddingTop: 25 }}
@@ -41,8 +32,10 @@ const SingleMealScreen = props => {
           <Text>{meal.title}</Text>
           <Text>{meal.price}</Text>
           
+          <TouchableOpacity>
           <Ionicons name="md-heart" size={32} color="red" />
           {/* md-heart-empty */}
+          </TouchableOpacity>
         </View>
       </View>
 
